7a2b568c6e2781583d7122bf7dfb6cc7e805bac6
Sulochan Mahajan <127333103+Sulochan36@users.noreply.github.com>
2025-04-22 04:54:32 +0530

    Time: 0 ms (100%), Space: 8.4 MB (99.76%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+
+    bool checkMatch(char c1, char c2){
+    if (c1 == '(' && c2 == ')') return true;
+    if (c1 == '[' && c2 == ']') return true;
+    if (c1 == '{' && c2 == '}') return true;
+    return false;
+}
+
+    bool isValid(string s) {
+       
+    int top = -1;
+    for (int i = 0; i < s.length(); ++i){
+      
+      
+        if (top < 0 || !checkMatch(s[top], s[i])){
+            ++top;
+            s[top] = s[i];
+        }
+        else{
+            --top;
+        }
+    }
+  
+    return top == -1;
+    }
+};
