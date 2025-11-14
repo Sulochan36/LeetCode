417ba0dd9f39988fb0161f3c2d51fa48fcfc0783
Sulochan Mahajan <127333103+Sulochan36@users.noreply.github.com>
2025-11-14 19:15:22 +0530

    Time: 0 ms (100%), Space: 14.3 MB (31.35%) - LeetHub

--- 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.cpp
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
     vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+         vector<int> count(101, 0);
+
+        // 1. Count frequency
+        for (int x : nums)
+            count[x]++;
+
+        // 2. Prefix sum
+        for (int i = 1; i < 101; i++)
+            count[i] += count[i - 1];
+
+        // 3. Build result
         vector<int> ans;
-        int count =0;
-        for(int i=0;i<nums.size();i++){
-            for(int j=0;j<nums.size();j++){
-                if(nums[j]<nums[i]){
-                    count++;
-                }
-            }
-            
-            ans.push_back(count);
-            count = 0;
+        ans.reserve(nums.size());
+
+        for (int x : nums) {
+            if (x == 0)
+                ans.push_back(0);
+            else
+                ans.push_back(count[x - 1]);
         }
 
         return ans;
